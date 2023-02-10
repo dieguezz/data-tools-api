4e7340d2c0c028e692ed04e553c1b0ffe46890d9
Diego Segura <diego@etereo.io>
2023-02-10 02:56:38 +0100

    whatever

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/dieguezz/nif-tools
 
 go 1.20
 
-require google.golang.org/grpc v1.53.0
+require (
+	google.golang.org/grpc v1.53.0
+	google.golang.org/protobuf v1.28.1
+)
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
@@ -10,5 +13,4 @@ require (
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
 	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
 )
--- server/main.go
+++ server/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	pb "github.com/dieguezz/nif-tools/proto"
 	"google.golang.org/grpc"
 	"log"
 	"net"
 )
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) GetControlDigit(ctx context.Context, in *pb.ControlDigitRequest) (*pb.ControlDigitRequest, error) {
+	return &pb.ControlDigitResponse{}, nil
+}
 
 func main() {
 	lis, err := net.Listen("tcp", ":9000")
@@ -14,6 +22,8 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	grpcServer := grpc.NewServer()
+	pb.RegisterNifApiServer(grpcServer, &server{})
+
 	if err := grpcServer.Serve(lis); ett != nil {
 		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
 	}
