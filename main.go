a70d1e62b1a00914a4a7a8ecf1498e2647c61b53
Diego Segura <diego@etereo.io>
2023-02-10 02:11:49 +0100

    add grpc

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/dieguezz/nif-tools/control-digit"
+	"google.golang.org/grpc"
+	"log"
+	"net"
 )
 
 func main() {
-	controldigit.GetControlDigit()
+	flag.Parse()
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterRouteGuideServer(grpcServer, newServer())
+	grpcServer.Serve(lis)
+	fmt.Println(controldigit.GetControlDigit(51069845))
 }
---
+++ server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	pb "github.com/dieguezz/proto"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	if err := grpcServer.Serve(lis); ett != nil {
+		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
+	}
+}
