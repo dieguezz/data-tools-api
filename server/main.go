124aebe0379a68572d5d69b40712fecd2ec2b2e7
Diego Segura <diego@etereo.io>
2023-02-10 23:15:18 +0100

    upload things

--- server/main.go
+++ server/main.go
@@ -1,42 +1,46 @@
 package main
 
 import (
-	"context"
+	// "context"
 	"github.com/dieguezz/nif-tools/control-digit"
-	pb "github.com/dieguezz/nif-tools/proto"
-	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
-	"google.golang.org/grpc"
-	"log"
-	"net"
-	"net/http"
+	// pb "github.com/dieguezz/nif-tools/proto"
+	// "github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	// "google.golang.org/grpc"
+	// "log"
+	// "net"
+	// "net/http"
 )
 
-type server struct {
-	pb.UnimplementedNifApiServer
-}
+// type server struct {
+// 	pb.UnimplementedNifApiServer
+// }
 
-func (s *server) GetControlDigit(ctx context.Context, in *pb.ControlDigitRequest) (*pb.ControlDigitResponse, error) {
-	controlDigit := controldigit.GetControlDigit(in.GetNif())
-	return &pb.ControlDigitResponse{ControlDigit: controlDigit}, nil
-
-}
+// func (s *server) GetControlDigit(ctx context.Context, in *pb.ControlDigitRequest) (*pb.ControlDigitResponse, error) {
+// 	controlDigit := controldigit.GetControlDigit(in.GetNif())
+// 	return &pb.ControlDigitResponse{ControlDigit: controlDigit}, nil
+// }
 
 func main() {
 
-	lis, err := net.Listen("tcp", ":9000")
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	grpcServer := grpc.NewServer()
-	go func() {
-		mux := runtime.NewServeMux()
-		pb.RegisterNifApiHandlerServer(context.Background(), mux, &server{})
-		log.Fatalln(http.ListenAndServe("localhost:8080", mux))
-	}()
+	// lis, err := net.Listen("tcp", ":9000")
+	// if err != nil {
+	// 	log.Fatalf("failed to listen: %v", err)
+	// }
+	// grpcServer := grpc.NewServer()
 
-	pb.RegisterNifApiServer(grpcServer, &server{})
+	// go func() {
+	// 	mux := runtime.NewServeMux()
+	// 	pb.RegisterNifApiHandlerServer(context.Background(), mux, &server{})
+	// 	log.Fatalln(http.ListenAndServe("localhost:8080", mux))
+	// }()
 
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
-	}
+	// pb.RegisterNifApiServer(grpcServer, &server{})
+
+	// if err := grpcServer.Serve(lis); err != nil {
+	// 	log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
+	// }
+	controldigit.GetControlDigit("L51069845r")
+	controldigit.GetControlDigit("x5555555")
+	controldigit.GetControlDigit("y5555555")
+	controldigit.GetControlDigit("z5555555")
 }
