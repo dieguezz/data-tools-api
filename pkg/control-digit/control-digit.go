d8c91587db0c6c236250d2b9be8d32dc36a28bcd
Diego Segura <diego@etereo.io>
2023-02-24 03:14:56 +0100

    :tada: add cif control digit

--- pkg/control-digit/control-digit.go
+++ pkg/control-digit/control-digit.go
@@ -1,6 +1,66 @@
 package nifcontroldigit
 
-func GetControlDigit(num int32) string {
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func GetNIFControlDigit(num int32) string {
 	remainder := map[int32]string{0: "T", 1: "R", 2: "W", 3: "A", 4: "G", 5: "M", 6: "Y", 7: "F", 8: "P", 9: "D", 10: "X", 11: "B", 12: "N", 13: "J", 14: "Z", 15: "S", 16: "Q", 17: "V", 18: "H", 19: "L", 20: "C", 21: "K", 22: "E"}
 	return remainder[num%23]
+}
+
+func GetCIFControlDigit(str string) string {
+	sumOfEven := 0
+	sumOfOdds := 0
+	firstChar := str[0:1]
+	parsedString := str[1:]
+
+	for i, char := range parsedString {
+		num, err := strconv.Atoi(string(char))
+		if err != nil {
+			log.Fatalf("Error converting to int: %v", err)
+		}
+
+		if i%2 != 0 {
+			sumOfEven += num
+		} else {
+			foo := fmt.Sprintf("%d", num*2)
+			subSum := 0
+			for _, subChar := range foo {
+				num, err := strconv.Atoi(string(subChar))
+				if err != nil {
+					log.Fatalf("Error converting to int: %v", err)
+				}
+
+				subSum += num
+			}
+
+			sumOfOdds += subSum
+		}
+	}
+
+	res := sumOfEven + sumOfOdds
+
+	result := 10 - res%10
+
+	control := ""
+	if strings.Contains("PQRSNW", firstChar) {
+		control = strings.ReplaceAll(fmt.Sprintf("%q", 64+result), "'", "")
+	} else {
+		if strings.Contains("ABCDEFGHJUV", firstChar) {
+			if result == 10 {
+				control = "0"
+			} else {
+				control = fmt.Sprintf("%v", result)
+			}
+		} else {
+			// I am not sure this case is correct
+			control = GetNIFControlDigit(int32(result))
+		}
+	}
+	return control
+
 }
--- pkg/control-digit/control-digit_test.go
+++ pkg/control-digit/control-digit_test.go
@@ -9,14 +9,32 @@ type nif struct {
 	control string
 }
 
-func TestGetControlDigit(t *testing.T) {
+type cif struct {
+	str     string
+	control string
+}
+
+func TestGetNIFControlDigit(t *testing.T) {
 	nifList := []nif{{number: 41509090, control: "R"}, {number: 40758223, control: "S"}, {number: 37909898, control: "X"}, {number: 74255828, control: "Y"}}
 
 	for _, x := range nifList {
-		result := GetControlDigit(int32(x.number))
+		result := GetNIFControlDigit(int32(x.number))
 
 		if result != x.control {
-			t.Fatal("Failed testing")
+			t.Errorf("Failed testing %v, expected %s, received %s", x.number, x.control, result)
+		}
+	}
+}
+
+func TestGetCIFControlDigit(t *testing.T) {
+	nifList := []cif{{str: "U3417318", control: "7"}, {str: "N0175417", control: "E"}, {str: "J4283343", control: "4"}, {str: "W5287701", control: "F"}}
+
+	for _, x := range nifList {
+		result := GetCIFControlDigit(x.str)
+
+		if result != x.control {
+			t.Errorf("Failed testing %v, expected %s, received %s", x.str, x.control, result)
+
 		}
 	}
 }
